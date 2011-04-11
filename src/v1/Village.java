6ee34c4cd430df45b3c2e2a76b2e7abcba63e278
Sindrus <sindreny@gmail.com>
2011-04-11 10:07:29 +0200

    flyttet buyFood og buySoldiers til Market klassen

--- src/v1/Village.java
+++ src/v1/Village.java
@@ -34,18 +34,4 @@ private boolean destroyed;
 	public void addSoldater(int soldier) {
 		this.soldater += soldier;
 	}
-	
-	public static void buySoldiers(Player p){
-		while (p.getGull() > 9) {
-			p.addSoldater(10);
-			p.addGull(-20);
-		}
-	}
-	
-	public static void buyFood(Player p){
-		while (p.getGull() > 9) {
-			p.addMat(5);
-			p.addGull(-10);
-		}
-	}
 }
